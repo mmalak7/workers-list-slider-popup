fbe27fa9962331fe165626ce476e6bf3085cc4d1
mmalak7 <marcinmalak7@gmail.com>
2019-10-26 19:25:47 +0200

    init webpack & react conf
    add gitignore too

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const settings = {
+    distPath: path.join(__dirname, "dist"),
+    srcPath: path.join(__dirname, "src")
+};
+
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'index_bundle.js',
+    },
+    module: {
+        rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ],
+            }
+        ]
+    },
+    devServer: {
+        historyApiFallback: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
+}
