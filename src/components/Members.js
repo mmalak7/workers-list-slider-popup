3d3338621ba17afdacaa9c1a9c743a116d730ba3
mmalak7 <marcinmalak7@gmail.com>
2019-10-27 13:07:13 +0100

    add membersList & reformat Members component
    make display of each member element shorter using map function

--- src/components/Members.js
+++ src/components/Members.js
@@ -2,6 +2,65 @@ import React, { Component } from 'react';
 import Card from '../models/CardModel';
 import '../styles/members/Members.scss';
 
+//static list used as substitute of connection between frontend and server
+//it could be easly change to fetch data from database, and no matter how many exist its easy 
+//to iterate throught them
+
+//TODO: //check approach desribed above
+
+const membersList = [
+    {
+        name: 'Basia Sołtysiak',
+        role: 'Founder',
+        location: 'Warsaw'
+    },
+    {
+        name: 'Włas Chorowiec',
+        role: 'Founder',
+        location: 'Warsaw',
+    },
+    {
+        name: 'Edyta Leśniewska',
+        role: 'Head of Human Resources',
+        location: 'Warsaw',
+    },
+    {
+        name: 'Joanna Pawluk',
+        role: 'Chef of growth Officer(CGO)',
+        location: 'London',
+    },
+    {
+        name: 'David Sunders',
+        role: 'Business Development',
+        location: 'London',
+    },
+    {
+        name: 'Adam Eldringe',
+        role: 'Business Developement Director US',
+        location: 'Los Angles',
+    },
+    {
+        name: 'Maha Mahda',
+        role: 'Chef Business Developement Office (CBDO)',
+        location: 'Warsaw',
+    },
+    {
+        name: 'Elżbieta Kamińska',
+        role: 'International Service Manager',
+        location: 'Warsaw',
+    },
+    {
+        name: 'Karolina Makuch',
+        role: 'Creation Manager',
+        location: 'Warsaw',
+    },
+    {
+        name: 'Kamil Bolek',
+        role: 'Head of communication',
+        location: 'Warsaw',
+    },
+]
+
 class Members extends Component {
     render() {
         return (
@@ -10,36 +69,13 @@ class Members extends Component {
                     <h1>Meet our team</h1>
                 </div>
                 <div className='members-container'>
-                    <div className='card'>
-                        <Card />
-                    </div>
-                    <div className='card'>
-                        <Card />
-                    </div>
-                    <div className='card'>
-                        <Card />
-                    </div>
-                    <div className='card'>
-                        <Card />
-                    </div>
-                    <div className='card'>
-                        <Card />
-                    </div>
-                    <div className='card'>
-                        <Card />
-                    </div>
-                    <div className='card'>
-                        <Card />
-                    </div>
-                    <div className='card'>
-                        <Card />
-                    </div>
-                    <div className='card'>
-                        <Card />
-                    </div>
-                    <div className='card'>
-                        <Card />
-                    </div>
+                    {membersList.map((value, index) => {
+                        return (
+                            <div className='card'>
+                                <Card name={value.name} role={value.role} location={value.location} />
+                            </div>
+                        )
+                    })}
                 </div>
             </div>
         )
--- src/models/CardModel.js
+++ src/models/CardModel.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import '../styles/card/Card.scss';
 
+
 const Card = props => {
     return (
         <div className='card__content'>
             <div className='img__part'>
-                <img src='https://source.unsplash.com/random/170x170' />
+                <img src='https://source.unsplash.com/random/150x150' />
                 <p className='card__img'></p>
             </div>
             <article className='card__text'>
-                <h2 id='card__name'>Basia Sołtysińska</h2>
-                <h2 id='card__role'>Founder</h2>
-                <h2 id='card__location'>Warsaw</h2>
+                <h2 id='card__name'>{props.name}</h2>
+                <h2 id='card__role'>{props.role}</h2>
+                <h2 id='card__location'>{props.location}</h2>
             </article>
         </div>
     );
